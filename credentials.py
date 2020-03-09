55da7a933c203492fff084c97fb851699ae8f8fd
James Lawrence <jlawrenc@stevens.edu>
2020-03-09 16:55:26 -0400

    test run code directory setup

---
+++ credentials.py
@@ -0,0 +1,23 @@
+user = "democlient"
+password = "password"
+
+
+"""
+#######################
+
+Competition user:
+two_and_twenty_llp
+
+Competition password:
+nXxS3qxkdLUTkkF9
+
+#######################
+
+Test users:
+test001 - test009
+
+Test password:
+password
+
+#######################
+"""
