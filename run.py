1b2bb780ed7e3e124de0903bb2fd7f81c4c7e1d4
James Lawrence <jlawrenc@stevens.edu>
2020-03-19 23:04:33 -0400

    new strategy added to run.py

--- run.py
+++ run.py
@@ -5,61 +5,78 @@ import numpy as np
 import datetime as dt
 import shift
 
+def portfolioSummary(trader: shift.Trader):
+    print("Buying Power\tTotal Shares\tTotal P&L\tTimestamp")
+    print(
+        "%12.2f\t%12d\t%9.2f\t%26s"
+        % (
+            trader.get_portfolio_summary().get_total_bp(),
+            trader.get_portfolio_summary().get_total_shares(),
+            trader.get_portfolio_summary().get_total_realized_pl(),
+            trader.get_portfolio_summary().get_timestamp(),
+        )
+    )
 
-def trafficLight(rightNow, dayStart, wait):
+    print()
 
-    if dayStart > rightNow:
-        print("Wait till market open")
-        time.sleep(wait)
-        print(dt.datetime.now())
-        trafficLight(dt.datetime.now(), dayStart, wait)
-    else:
-        print("Begin Trading")
-        return
+    print("Symbol\t\tShares\t\tPrice\t\t  P&L\tTimestamp")
+    for item in trader.get_portfolio_items().values():
+        print(
+            "%6s\t\t%6d\t%9.2f\t%9.2f\t%26s"
+            % (
+                item.get_symbol(),
+                item.get_shares(),
+                item.get_price(),
+                item.get_realized_pl(),
+                item.get_timestamp(),
+            )
+        )
 
-
-def closePositions(trader: shift.Trader, ticker, lastTrade):
-
-    # Cancel pending orders
-    print("Waiting list size: " + str(trader.get_waiting_list_size()) + " , Canceling all pending orders...")
-    trader.cancel_all_pending_orders()
-    print("Waiting list size: " + str(trader.get_waiting_list_size()))
-    print("All pending orders cancelled") # Cancel outstanding open orders before entering closing orders
-
-
-    # Close / Cover all open positions
-    """
-    Either sell at market or at best bid w/ volume required
-    """
-    """
-    if lastTrade is 'B': # Close out long position
-        aapl_market_sell = shift.Order(shift.Order.Type.MARKET_SELL, ticker, 1)
-        trader.submit_order(aapl_market_sell)
-    else: # Cover short position
-        aapl_market_buy = shift.Order(shift.Order.Type.MARKET_BUY, ticker, 1)
-        trader.submit_order(aapl_market_buy)
-    """
-    print("**TODO** All closing orders submitted")
+    print()
 
     return
 
 
-def moneyMaker(trader: shift.Trader, ticker, dayEnd, lag):
+def closePositions(trader: shift.Trader):
 
-    # Datetime beginning trading
-    rightNow = dt.datetime.now()
+    # Cancel pending orders
+    print("Waiting list size: " + str(trader.get_waiting_list_size()) + " , Canceling all pending orders...")
+    for order in trader.get_waiting_list():
+        trader.submit_cancellation(order)
+    print("All pending orders cancelled") # Cancel outstanding open orders before entering closing orders
+
+
+    # Close / Cover all open positions
+    portfolioSummary(trader) # Print summary of portfolio
+    for item in trader.get_portfolio_items().values():
+        if item.get_shares() > 0:
+            print("Open long positions")
+            closeLong = shift.Order(shift.Order.Type.MARKET_SELL, item.get_symbol(), int(item.get_shares() / 100)) # Order size in 100's of shares, strictly as an int
+            trader.submit_order(closeLong)
+        if item.get_shares() < 0:
+            print("Open short positions")
+            coverShort = shift.Order(shift.Order.Type.MARKET_BUY, item.get_symbol(), int(item.get_shares() / -100))
+            trader.submit_order(coverShort)
+    print("All closing orders submitted")
+
+
+    return
+
+
+def firstStrategy(trader: shift.Trader, ticker, dayEnd, lag):
+
+    # Datetime of simulation
+    rightNow =  trader.get_last_trade_time()
 
     # FIFO queue initialized
     priceQueue = []
 
-    currentDirection = 0
     signal = 'S'
-
     # While the time is before end of day...
     while(dayEnd > rightNow):
         # Pause and wait for change in prices
         time.sleep(lag)
-        print("Total P/L:",trader.get_portfolio_summary().get_total_realized_pl())
+        print(rightNow, "Total P/L:",trader.get_portfolio_summary().get_total_realized_pl())
         """
         Make Trades Here:
         """
@@ -90,10 +107,57 @@ def moneyMaker(trader: shift.Trader, ticker, dayEnd, lag):
             priceQueue.append(trader.get_last_price(ticker)) # Fill the queue from oldest price to latest price
 
 
-        rightNow = dt.datetime.now() # Reset time rightNow
+        rightNow =  trader.get_last_trade_time() # Reset datetime of right now
 
     # 90 seconds till end of trading day
-    closePositions(trader, ticker, signal)
+    closePositions(trader)
+
+    # Done trading
+    return
+
+
+def marketMaker(trader: shift.Trader, ticker, dayEnd, lag):
+
+    # Datetime of simulation
+    rightNow =  trader.get_last_trade_time()
+
+    count = 1
+    # While the time is before end of day...
+    while(dayEnd > rightNow):
+        print(rightNow, "Total P/L:",trader.get_portfolio_summary().get_total_realized_pl())
+        """
+        Make Trades Here:
+        """
+        if count % ((1/lag)*10) == 0: # Every so often, sell off inventory.  Wait longer if lower lag
+        	closePositions(trader)
+        	print("Sold inventory, risk reset")
+
+        time.sleep(lag) # Give prices some time to change
+
+        # Submit a buy order
+        limit_buy = shift.Order(shift.Order.Type.LIMIT_BUY, ticker, 1, trader.get_best_price(ticker).get_bid_price()) # Can buy +0.5 above bid with wide spread, or -.01 below if high volume
+        trader.submit_order(limit_buy)
+        print("Bought")
+
+        time.sleep(0.1) # Allow a brief time for the order to fill
+
+        # Sell if the buy order filled
+        if trader.get_order(limit_buy.id).status == shift.Order.Status.FILLED:
+        	limit_sell = shift.Order(shift.Order.Type.LIMIT_SELL, ticker, 1, trader.get_best_price(ticker).get_ask_price()) # Can sell -.05 below ask with wide spread, or +.01 above if high volume
+        	trader.submit_order(limit_sell)
+        	print("Filled buy and Sold")
+
+        # Otherwise cancel the buy order
+        else:
+            for order in trader.get_waiting_list():
+                trader.submit_cancellation(order)
+            print("Cancelled")
+
+        count = count + 1 # Increment counter
+        rightNow =  trader.get_last_trade_time() # Reset datetime of right now
+
+    # 60 seconds till end of trading day
+    closePositions(trader)
 
     # Done trading
     return
@@ -113,29 +177,23 @@ def main(argv):
     except shift.ConnectionTimeoutError as e:
         print(e)
 
-    # Today's date
-    today = dt.date.today()
-
-    # Datetime right now
-    rightNow = dt.datetime.now()
-
-    # Start of trading day datetime
-    startTime = dt.time(10,30,00) # **Competition time******************************
-    #startTime = dt.time(22,53,00) # **Set time for development**
-    dayStart = dt.datetime.combine(today,startTime)
-
-    # Wait to begin trading
-    trafficLight(rightNow, dayStart, 0.5)
+    time.sleep(.5)
+    # Date of simulation
+    today = trader.get_last_trade_time().date()
 
     # End of trading day datetime
-    endTime = dt.time(16,58,30) # **Competition time********************************
-    #endTime = dt.time(23,56,30) # **Set time for development**
+    endTime = dt.time(16,59,0) # **Competition time********************************
+    #endTime = dt.time(10,11,30) # **Set time for development**
     dayEnd = dt.datetime.combine(today,endTime)
 
     # Begin trading
     print("Initial buying power:",trader.get_portfolio_summary().get_total_bp())
-    ticker = "SPY"
-    moneyMaker(trader, ticker, dayEnd, 5.0)
+
+    #ticker = "SPY"
+    #firstStrategy(trader, ticker, dayEnd, 5.0)
+
+    ticker = "SPY" # "BA"
+    marketMaker(trader, ticker, dayEnd, .4)
     
     # Disconnect
     print("Final buying power:",trader.get_portfolio_summary().get_total_bp())
@@ -146,20 +204,3 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
-
-
-"""
-time checks during simulation:
-get_last_trade_time()
-
-can assume moment code starts we can trade -- use get_last_trade_time() to
-track time of day, so we can close the open positions
-
-
-for item in trader.get_portfolio_items().values()
-    if item.get_shares() > 0:
-        trader.submit_order(shift.Order(shift.Order.Type.MARKET_SELL, item.get_symbol(), item.get_shares() / 100) # Order size in 100's of shares
-    if item.get_shares() < 0:
-        trader.submit_order(shift.Order(shift.Order.Type.MARKET_BUY, item.get_symbol(), item.get_shares() / (-100))
-
-"""
